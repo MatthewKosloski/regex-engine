c480918ef614d9392341f4f7ecf9a5b5f22f4f59
Matthew Kosloski <matthewkosloski@me.com>
2023-09-12 23:23:37 -0500

    updated comments

--- src/parser/Grammar.ts
+++ src/parser/Grammar.ts
@@ -11,6 +11,7 @@ class Grammar extends Parser {
     public parse(): Expr {
         // regex -> expr* ;
         while (this.hasTokens()) {
+            // expr*
             this.expr.addChild(this.parseExpr());
         }
         return this.expr;
@@ -19,37 +20,91 @@ class Grammar extends Parser {
     /**
      * Parses an expression.
      * 
-     * Grammar rule: expr -> symbol_expr
-     *                | parenthesized_expr
-     *                | kleene_expr
-     *                | concat_expr
-     *                | alternation_expr ;
+     * Grammar rule:
+     *  `expr -> alt_expr ;`
      * 
-     * @return The parsed expression as an AST node. 
+     * @returns The parsed expression as an AST node. 
      */
     private parseExpr() {
+        // alt_expr
         return this.parseAlternationExpr();
     }
 
     /**
-     * Parses a parenthesized expression.
+     * Parses an alternation expression.
      * 
-     * Grammar rule: symbol_expr -> ( "" | [a-z] | [A-Z] | [0-9] )* ;
+     * Grammar rule:
+     *   `alt_expr -> (concat_expr "|" concat_expr) | concat_expr ;`
      * 
-     * @return The parsed parenthesized expression as an AST node. 
+     * @returns The parsed alternation expression as an AST node. 
      */
-    private parseCharacterExpr() {
-        if (this.peek()?.type === TokenType.Char) {
-            return new Expr(ExprType.Character, this.match(TokenType.Char));
+    private parseAlternationExpr() {
+        if (this.peekNext()?.type === TokenType.Pipe) {
+            // concat_expr
+            const left = this.parseConcatExpr();
+
+            // "|"
+            const tok = this.match(TokenType.Pipe);
+
+            // concat_expr
+            const right = this.parseConcatExpr();
+
+            return new Expr(ExprType.Alternation, tok, left, right);
+        } else {
+            // concat_expr
+            return this.parseConcatExpr();
+        }
+    }
+
+    /**
+     * Parses a concatenation expression.
+     * 
+     * Grammar rule:
+     *   `concat_expr -> kleene_expr+ ;`
+     * 
+     * @returns The parsed alternation expression as an AST node. 
+     */
+    private parseConcatExpr() {
+        const expr = new Expr(ExprType.Concatenation);
+
+        // kleene_expr+
+        do {
+            expr.addChild(this.parseKleeneStarExpr());
+        } while (!(this.isTokenOperator() || this.peek()?.type === TokenType.EndOfFile))
+
+        if (expr.children.length === 1) {
+            expr.addChild(new Expr(ExprType.Erasure));
         }
 
-        throw new Error('Failed to parse.');
+        return expr;
+    }
+
+    /**
+     * Parses a Kleene Star expression.
+     * 
+     * Grammar rule: kleene_expr -> paren_expr "*" | paren_expr ;
+     * 
+     * @return The parsed Kleene Star expression as an AST node. 
+     */
+    private parseKleeneStarExpr() {
+        if (this.peekNext()?.type === TokenType.Star) {
+            // paren_expr
+            const operand = this.parseParenthesizedExpr();
+
+            // "*"
+            const tok = this.match(TokenType.Star);
+
+            return new Expr(ExprType.KleeneStar, tok, operand);
+        } else {
+            // paren_expr
+            return this.parseParenthesizedExpr();
+        }
     }
 
     /**
      * Parses a parenthesized expression.
      * 
-     * Grammar rule: parenthesized_expr -> "(" expr+ ")" ;
+     * Grammar rule: paren_expr -> "(" expr+ ")" | char_expr ;
      * 
      * @return The parsed parenthesized expression as an AST node. 
      */
@@ -69,56 +124,29 @@ class Grammar extends Parser {
 
             return expr;
         } else {
+            // char_expr
             return this.parseCharacterExpr();
         }
     }
 
     /**
-     * Parses a Kleene Star expression.
+     * Parses a parenthesized expression.
      * 
-     * Grammar rule: kleene_expr -> expr "*" ;
+     * Grammar rule: char_expr -> [a-z] | [A-Z] | [0-9] ;
      * 
-     * @return The parsed Kleene Star expression as an AST node. 
+     * @return The parsed parenthesized expression as an AST node. 
      */
-    private parseKleeneStarExpr() {
-        if (this.peekNext()?.type === TokenType.Star) {
-            const operand = this.parseParenthesizedExpr();
-            return new Expr(ExprType.KleeneStar, this.match(TokenType.Star), operand);
-        } else {
-            return this.parseParenthesizedExpr();
-        }
-    }
+    private parseCharacterExpr() {
+        if (this.peek()?.type === TokenType.Char) {
+            const tok = this.match(TokenType.Char);
 
-    /**
-     * Parses an alternation expression.
-     * 
-     * @return The parsed alternation expression as an AST node. 
-     */
-    private parseAlternationExpr() {
-    if (this.peekNext()?.type === TokenType.Pipe) {
-        const left = this.parseConcatExpr();
-        const tok = this.match(TokenType.Pipe);
-        const right = this.parseConcatExpr();
-
-        return new Expr(ExprType.Alternation, tok, left, right);
-    } else {
-            return this.parseConcatExpr();
-        }
-    }
-
-    private parseConcatExpr() {
-        const expr = new Expr(ExprType.Concatenation);
-
-        // kleene_expr+
-        do {
-            expr.addChild(this.parseKleeneStarExpr());
-        } while (!(this.isTokenOperator() || this.peek()?.type === TokenType.EndOfFile))
-
-        if (expr.children.length === 1) {
-            expr.addChild(new Expr(ExprType.Erasure));
+            return new Expr(ExprType.Character, tok);
         }
 
-        return expr;
+        // If we reach this point, then the regular expression
+        // has a syntax error.
+
+        throw new Error('Failed to parse.');
     }
 
 }
